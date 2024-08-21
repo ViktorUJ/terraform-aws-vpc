6eb1d860d3688071faa73bac6024aebc73f221f8
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 00:09:35 +0300

    ~

---
+++ outputs.tf
@@ -0,0 +1,69 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = try(aws_vpc.default.id, null)
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = try(aws_vpc.default.arn, null)
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = try(aws_vpc.default.cidr_block, null)
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = try(aws_vpc.default.default_security_group_id, null)
+}
+
+output "default_network_acl_id" {
+  description = "The ID of the default network ACL"
+  value       = try(aws_vpc.default.default_network_acl_id, null)
+}
+
+output "default_route_table_id" {
+  description = "The ID of the default route table"
+  value       = try(aws_vpc.default.default_route_table_id, null)
+}
+
+output "vpc_instance_tenancy" {
+  description = "Tenancy of instances spin up within VPC"
+  value       = try(aws_vpc.default.instance_tenancy, null)
+}
+
+output "vpc_enable_dns_support" {
+  description = "Whether or not the VPC has DNS support"
+  value       = try(aws_vpc.default.enable_dns_support, null)
+}
+
+output "vpc_enable_dns_hostnames" {
+  description = "Whether or not the VPC has DNS hostname support"
+  value       = try(aws_vpc.default.enable_dns_hostnames, null)
+}
+
+output "vpc_main_route_table_id" {
+  description = "The ID of the main route table associated with this VPC"
+  value       = try(aws_vpc.default.main_route_table_id, null)
+}
+
+output "vpc_ipv6_association_id" {
+  description = "The association ID for the IPv6 CIDR block"
+  value       = try(aws_vpc.default.ipv6_association_id, null)
+}
+
+output "vpc_ipv6_cidr_block" {
+  description = "The IPv6 CIDR block"
+  value       = try(aws_vpc.default.ipv6_cidr_block, null)
+}
+
+output "vpc_secondary_cidr_blocks" {
+  description = "List of secondary CIDR blocks of the VPC"
+  value       = compact(aws_vpc_ipv4_cidr_block_association.default[*].cidr_block)
+}
+
+output "vpc_owner_id" {
+  description = "The ID of the AWS account that owns the VPC"
+  value       = try(aws_vpc.default.owner_id, null)
+}
