b631f7e8788fc61f64b132679ecc238793e6d17e
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 00:25:33 +0300

    ~

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "tags_default" {
 
 
 variable "subnets" {
-  type = object({
+  type = optional(object({
     public=optional(map(object({
       name=string
       cidr=string
@@ -32,7 +32,7 @@ variable "subnets" {
       tags=optional(map(string), {})
       type=optional(string, "private") # any sort key for grouping . example , DB , WEB , APP , etc
     })))
-  })
+  }))
 }
 
 
