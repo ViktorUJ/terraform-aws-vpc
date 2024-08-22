280d2d3bf161b0a0ab2214887b30c0d7a0692be1
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 21:51:54 +0300

    test

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -14,7 +14,13 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   secondary_cidr_blocks=["10.2.0.0/16","10.3.0.0/16"]
   subnets={
-
+  public ={
+    "pub1"={
+      name="public subnet 1"
+      cidr="10.0.0.0/24"
+      az="eun1-az1"
+           }
+  }
   }
   }
 
