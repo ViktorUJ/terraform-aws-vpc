38cbbbf196c38f877fdd402ba17f225441b89b12
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 22:30:45 +0300

    test

--- subnets_pub.tf
+++ subnets_pub.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "subnets_pub" {
+resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.default.id
   for_each                = var.subnets.public
   map_public_ip_on_launch = true
@@ -7,11 +7,17 @@ resource "aws_subnet" "subnets_pub" {
   tags                    = merge(var.tags_default , { "Name" = each.value.name }, {"type"=each.value.type}, {"subnet_key"=each.key},{"access_type"="public"} ,each.value.tags )
 }
 
-resource "aws_route_table" "pub" {
+resource "aws_route_table" "public" {
   vpc_id     = aws_vpc.default.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.default.id
   }
     tags  = merge(var.tags_default , {"access_type"="public"} )
+}
+
+resource "aws_route_table_association" "pub" {
+  for_each       = var.subnets.public
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public["${each.key}"].id
 }
