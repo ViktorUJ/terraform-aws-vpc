036b28b61cb938b95ff4551be8093adfebd42fa7
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 22:27:24 +0300

    test

--- subnets_pub.tf
+++ subnets_pub.tf
@@ -6,3 +6,12 @@ resource "aws_subnet" "subnets_pub" {
   availability_zone_id    = each.value.az
   tags                    = merge(var.tags_default , { "Name" = each.value.name }, {"type"=each.value.type}, {"subnet_key"=each.key},{"access_type"="public"} ,each.value.tags )
 }
+
+resource "aws_route_table" "pub" {
+  vpc_id     = aws_vpc.default.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+    tags  = merge(var.tags_default , {"access_type"="public"} )
+}
