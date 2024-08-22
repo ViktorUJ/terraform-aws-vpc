2e05ff4a9759c4fb3c4f9350db33ecfa026122e7
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 21:30:29 +0300

    test

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
   vpc={
   name = "test-vpc"
   cidr = "10.0.0.0/16"
-  secondary_cidr_blocks=["10.1.0.0/16","10.2.0.0/16","10.3.0.0/16"]
+  secondary_cidr_blocks=["10.2.0.0/16","10.3.0.0/16"]
   subnets={
 
   }
