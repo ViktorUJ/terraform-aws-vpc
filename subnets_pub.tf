7f50cf59a59fd8e021118f4cc3f0a7a109663e42
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 21:47:18 +0300

    test

---
+++ subnets_pub.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "subnets_pub" {
+  vpc_id                  = aws_vpc.default.id
+  for_each                = var.subnets.public
+  map_public_ip_on_launch = true
+  cidr_block              = each.value.cidr
+  availability_zone_id    = each.value.az
+  tags                    = merge(var.tags_default,{ "Name" = each.value.name }, {"type"=each.value.type}, {"subnet_key"=each.key})
+}
