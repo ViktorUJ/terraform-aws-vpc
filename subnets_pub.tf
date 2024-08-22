1ad2ac91153204f10bc61e503f5281b4f6c84640
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-22 23:46:49 +0300

    ~

--- subnets_pub.tf
+++ subnets_pub.tf
@@ -1,8 +1,20 @@
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.default.id
   for_each                = var.subnets.public
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = each.value.map_public_ip_on_launch
   cidr_block              = each.value.cidr
+
+  assign_ipv6_address_on_creation = each.value.assign_ipv6_address_on_creation
+  customer_owned_ipv4_pool = each.value.customer_owned_ipv4_pool != "" ? each.value.customer_owned_ipv4_pool : null
+  enable_dns64= each.value.enable_dns64
+  enable_resource_name_dns_aaaa_record_on_launch= each.value.enable_resource_name_dns_aaaa_record_on_launch
+  enable_resource_name_dns_a_record_on_launch= each.value.enable_resource_name_dns_a_record_on_launch
+  ipv6_cidr_block= each.value.ipv6_cidr_block != ""? each.value.ipv6_cidr_block : null
+  ipv6_native= each.value.ipv6_native
+  map_customer_owned_ip_on_launch= each.value.map_customer_owned_ip_on_launch
+  outpost_arn= each.value.outpost_arn != "" ? each.value.outpost_arn : null
+  private_dns_hostname_type_on_launch= each.value.private_dns_hostname_type_on_launch != "" ? each.value.private_dns_hostname_type_on_launch : null
+
 
   availability_zone_id    = length(regexall("^[a-z]{2}-", each.value.az)) == 0 ?  each.value.az :null
   availability_zone       = length(regexall("^[a-z]{2}-", each.value.az)) > 0 ? each.value.az : null
--- variables.tf
+++ variables.tf
@@ -24,6 +24,20 @@ variable "subnets" {
       az=string # Availability Zone or Availability Zone ID
       tags=optional(map(string), {})
       type=optional(string, "public") # any sort key for grouping . example , DB , WEB , APP , etc
+
+      assign_ipv6_address_on_creation = optional(bool, false)
+      customer_owned_ipv4_pool = optional(string, "")
+      enable_dns64= optional(bool, false)
+      enable_resource_name_dns_aaaa_record_on_launch= optional(bool, false)
+      enable_resource_name_dns_a_record_on_launch= optional(bool, false)
+      ipv6_cidr_block= optional(string, "")
+      ipv6_native= optional(bool, false)
+      map_customer_owned_ip_on_launch= optional(bool, false)
+      map_public_ip_on_launch= optional(bool, true)
+      outpost_arn= optional(string, "")
+      private_dns_hostname_type_on_launch= optional(string, "")
+
+
     })))
    private=optional(map(object({
       name=string
