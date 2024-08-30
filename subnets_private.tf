2fd8bcbff1521156335b6c6cb167a1cc58e22237
Viktar Mikalayeu <viktoruj@gmail.com>
2024-08-30 23:55:48 +0400

    ~

--- subnets_private.tf
+++ subnets_private.tf
@@ -19,5 +19,15 @@ resource "aws_subnet" "private" {
   availability_zone_id    = length(regexall("^[a-z]{2}-", each.value.az)) == 0 ?  each.value.az :null
   availability_zone       = length(regexall("^[a-z]{2}-", each.value.az)) > 0 ? each.value.az : null
 
-  tags                    = merge(var.tags_default , { "Name" = each.value.name }, {"type"=each.value.type}, {"subnet_key"=each.key},{"access_type"="public"} ,each.value.tags )
+  tags                    = merge(var.tags_default , { "Name" = each.value.name }, {"type"=each.value.type}, {"subnet_key"=each.key},{"access_type"="private"} ,each.value.tags )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id     = aws_vpc.default.id
+  for_each                = var.subnets.private
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_internet_gateway.default.id
+#  }
+    tags                    = merge(var.tags_default , { "Name" = each.value.name }, {"type"=each.value.type}, {"subnet_key"=each.key},{"access_type"="private"} ,each.value.tags )
 }
