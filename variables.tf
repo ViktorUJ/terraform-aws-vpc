bbe8ff3992d0d16cce4e6d33275714c25f235d6b
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-08 21:41:48 +0400

    ~

--- variables.tf
+++ variables.tf
@@ -25,9 +25,8 @@ variable "vpc" {
         domain_name_servers  = optional(list(string), [])
         ntp_servers          = optional(list(string), [])
         netbios_name_servers = optional(list(string), [])
-        netbios_node_type    = optional(list(string), [])
-        ipv6_address_preferred_lease_time= optional(string, "") # 140 .. 2147483647 seconds . default 140
         netbios_node_type= optional(string, "") # 1, 2, 4, 8  . default 2 . http://www.ietf.org/rfc/rfc2132.txt
+        ipv6_address_preferred_lease_time= optional(string, "") # 140 .. 2147483647 seconds . default 140
         tags                 = optional(map(string), {})
     }), {})
   })
