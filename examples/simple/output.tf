f5b0cb69d9e9700d7459698759cb7a8829a150f6
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-08 17:13:04 +0400

    ~

--- examples/simple/output.tf
+++ examples/simple/output.tf
@@ -117,58 +117,11 @@ output "vpc_cidr_block" {
 }
 
 output "k8s_private_subnets" {
+  description = "K8s private subnets"
   value = module.vpc.private_subnets_by_type.k8s.ids
 }
 
-/*
-output "vpc_id" {
-  value = module.vpc.vpc_raw.id
+output "private_subnets_az" {
+  description = "privare subnets in AZ"
+  value = module.vpc.private_subnets_by_az.eun1-az3.ids
 }
-
-output "vpc_cidr_block" {
-  value = module.vpc.vpc_raw.cidr_block
-}
-
-output "vpc_raw" {
-  value = module.vpc.vpc_raw
-}
-
-
-output "subnets_var" {
-  value = module.vpc.subnets_var
-}
-output "subnets_public_raw" {
-  value = module.vpc.subnets_public_raw
-}
-output "az_mapping" {
-  value = module.vpc.az_mapping
-}
-output "normalized_subnet" {
-  value = module.vpc.normalized_private_subnets_all
-}
-
-
-output "private_subnets_by_type" {
-  value = module.vpc.private_subnets_by_type
-}
-
-output "private_subnets_by_az" {
-  value = module.vpc.private_subnets_by_az
-}
-output "private_subnets_by_az_id" {
-  value = module.vpc.private_subnets_by_az_id
-}
-
-
-output "public_subnets_by_type" {
-  value = module.vpc.public_subnets_by_type
-}
-
-output "public_subnets_by_az" {
-  value = module.vpc.public_subnets_by_az
-}
-output "public_nacl_rules" {
-  value = module.vpc.public_nacl_rules
-}
-
-*/
