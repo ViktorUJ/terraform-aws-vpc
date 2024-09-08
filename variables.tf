d927dc9623af86e25583220dd6f2285ca7ccff44
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-08 22:25:55 +0400

    ~

--- variables.tf
+++ variables.tf
@@ -1,49 +1,54 @@
 variable "vpc" {
   type = object({
-    name                  = string
-    cidr                  = string
+    name = string
+    cidr = string
     secondary_cidr_blocks = optional(list(string), [])
-    tags                  = optional(map(string), {})
-    instance_tenancy      = optional(string, "default") # default, dedicated
-    enable_dns_support    = optional(bool, true)        # true, false
-    enable_dns_hostnames  = optional(bool, false)       # true, false
+    tags = optional(map(string), {})
+    instance_tenancy = optional(string, "default") # default, dedicated
+    enable_dns_support = optional(bool, true)        # true, false
+    enable_dns_hostnames = optional(bool, false)       # true, false
     nacl_default = optional(map(object({
-      egress          = string # true, false
-      rule_number     = string # ACL entries are processed in ascending order by rule number
-      rule_action     = string # allow | deny
-      from_port       = optional(string, "")
-      to_port         = optional(string, "")
-      icmp_code       = optional(string, "") # (Optional) ICMP protocol: The ICMP type. Required if specifying ICMP for the protocolE.g., -1
-      icmp_type       = optional(string, "") # (Optional) ICMP protocol: The ICMP code. Required if specifying ICMP for the protocolE.g., -1
-      protocol        = string               # A value of -1 means all protocols , tcp  - 6 ,
-      cidr_block      = optional(string, "") # The network range to allow or deny, in CIDR notation (for example 172.16.0.0/24 ).
+      egress = string # true, false
+      rule_number = string # ACL entries are processed in ascending order by rule number
+      rule_action = string # allow | deny
+      from_port = optional(string, "")
+      to_port = optional(string, "")
+      icmp_code = optional(string, "")
+      # (Optional) ICMP protocol: The ICMP type. Required if specifying ICMP for the protocolE.g., -1
+      icmp_type = optional(string, "")
+      # (Optional) ICMP protocol: The ICMP code. Required if specifying ICMP for the protocolE.g., -1
+      protocol = string               # A value of -1 means all protocols , tcp  - 6 ,
+      cidr_block = optional(string, "")
+      # The network range to allow or deny, in CIDR notation (for example 172.16.0.0/24 ).
       ipv6_cidr_block = optional(string, "")
 
     })), {})
-    dhcp_options= optional(object({
-        domain_name          = optional(string, "")
-        domain_name_servers  = optional(list(string), [])
-        ntp_servers          = optional(list(string), [])
-        netbios_name_servers = optional(list(string), [])
-        netbios_node_type= optional(string, "") # 1, 2, 4, 8  . default 2 . http://www.ietf.org/rfc/rfc2132.txt
-        ipv6_address_preferred_lease_time= optional(string, "") # 140 .. 2147483647 seconds . default 140
-        tags                 = optional(map(string), {})
+    dhcp_options = optional(object({
+      domain_name = optional(string, "")
+      domain_name_servers = optional(list(string), [])
+      ntp_servers = optional(list(string), [])
+      netbios_name_servers = optional(list(string), [])
+      netbios_node_type = optional(string, "") # 1, 2, 4, 8  . default 2 . http://www.ietf.org/rfc/rfc2132.txt
+      ipv6_address_preferred_lease_time = optional(string, "") # 140 .. 2147483647 seconds . default 140
+      tags = optional(map(string), {})
     }), {})
   })
 
   # Validation for CIDR format
   validation {
-    condition     = can(cidrsubnet(var.vpc.cidr, 0, 0))
+    condition = can(cidrsubnet(var.vpc.cidr, 0, 0))
     error_message = "Invalid CIDR block format for VPC. CIDR block must be a valid subnet, e.g., 10.0.0.0/16."
   }
 
-   # Validation for netbios_node_type field
+  # Validation for netbios_node_type field
   validation {
-    condition     = var.vpc.dhcp_options.netbios_node_type == "" || contains(["1", "2", "4", "8"], var.vpc.dhcp_options.netbios_node_type)
+    condition     = var.vpc.dhcp_options.netbios_node_type == "" || contains([
+      "1", "2", "4", "8"
+    ], var.vpc.dhcp_options.netbios_node_type)
     error_message = "Invalid value for netbios_node_type. Must be one of: 1, 2, 4, 8."
   }
 
-  # Validation for ipv6_address_preferred_lease_time
+    # Validation for ipv6_address_preferred_lease_time
   validation {
     condition = var.vpc.dhcp_options.ipv6_address_preferred_lease_time == "" ||
       (
@@ -53,6 +58,7 @@ variable "vpc" {
       )
     error_message = "Invalid value for ipv6_address_preferred_lease_time. Must be a number between 140 and 2147483647 seconds."
   }
+
 }
 
 variable "tags_default" {
