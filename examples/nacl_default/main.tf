d16aecc98154a2d7aae725cdc49212b4cee81a92
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-09 09:20:01 +0400

    ~

---
+++ examples/nacl_default/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+
+module "vpc" {
+  source = "../../"
+  tags_default = {
+    "Owner"       = "DevOps Team"
+    "Terraform"   = "true"
+    "cost_center" = "1111"
+  }
+  vpc = {
+    name                  = "test-vpc"
+    cidr                  = "10.10.0.0/16"
+    nacl_default = {
+      test = {
+        egress      = "true"
+        rule_number = "99"
+        rule_action = "allow"
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+
+
+      }
+      test2 = {
+        egress      = "false"
+        rule_number = "99"
+        rule_action = "allow"
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+
+
+      }
+    }
+  }
+
+  subnets = {}
+}
