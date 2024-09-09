4b1575336f01b8d6959246c6a639e4caddeb578b
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-09 09:10:39 +0400

    ~

--- examples/output_private_subnet_by_type/output.tf
+++ examples/output_private_subnet_by_type/output.tf
@@ -5,3 +5,11 @@ output "private_subnets_by_type" {
 output "k8s_subnets_id" {
   value = module.vpc.private_subnets_by_type.k8s.ids
 }
+
+output "rds_subnets_id" {
+  value = module.vpc.private_subnets_by_type.rds.ids
+}
+
+output "app_subnets_id" {
+  value = module.vpc.private_subnets_by_type.app.ids
+}
