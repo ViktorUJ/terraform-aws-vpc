1d2c17f14d39b350f11d35fcfffd20f4bdd2035d
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-09 10:50:36 +0400

    ~

--- examples/nacl_subnet/output.tf
+++ examples/nacl_subnet/output.tf
@@ -1,15 +1,7 @@
-output "vpc_id" {
-  value = module.vpc.vpc_raw.id
+output "private_nacl_raw" {
+  value = module.vpc.private_nacl_raw
 }
 
-output "vpc_cidr_block" {
-  value = module.vpc.vpc_raw.cidr_block
-}
-
-
-output "private_subnets_by_type" {
-  value = module.vpc.private_subnets_by_type
-}
-output "public_subnets_by_type" {
-  value = module.vpc.public_subnets_by_type
+output "private_nacl_rules_raw" {
+  value = module.vpc.private_nacl_rules_raw
 }
