004f075e9af34b0bdcc0c1e4c627967821e0fc54
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-30 21:10:50 +0400

    ~

--- examples/nat_gateway_az/main.tf
+++ examples/nat_gateway_az/main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 
 
 module "vpc" {
-  source = "ViktorUJ/vpc/aws"
+#  source = "ViktorUJ/vpc/aws"
+    source = "../../"
   tags_default = {
     "Owner"       = "DevOps Team"
     "Terraform"   = "true"
@@ -25,38 +26,38 @@ module "vpc" {
       "pub2" = {
         name = "public-subnet-2"
         cidr = "10.10.2.0/24"
-        az   = "eu-north-1a"
+        az   = "eu-north-1b"
       }
 
     }
     private = {
 
-      "private1" = {
-        name        = "private-subnet-1"
-        cidr        = "10.10.11.0/24"
-        az          = "eu-north-1a"
-        nat_gateway = "AZ" # it is default . you can remove it
-
-      }
-      "private2" = {
-        name = "private-subnet-2"
-        cidr = "10.10.12.0/24"
-        az   = "eu-north-1a"
-        #       nat_gateway                         = "AZ"  # it is default . you can remove it
-      }
-      "private3" = {
-        name = "private-subnet-3"
-        cidr = "10.10.13.0/24"
-        az   = "eu-north-1b"
-        #       nat_gateway                         = "AZ"  # it is default . you can remove it
-      }
-      "private4" = {
-        name = "private-subnet-4"
-        cidr = "10.10.14.0/24"
-        az   = "eu-north-1b"
-        #       nat_gateway                         = "AZ"  # it is default . you can remove it
-      }
-
+#      "private1" = {
+#        name        = "private-subnet-1"
+#        cidr        = "10.10.11.0/24"
+#        az          = "eu-north-1a"
+#        nat_gateway = "AZ" # it is default . you can remove it
+#
+#      }
+#      "private2" = {
+#        name = "private-subnet-2"
+#        cidr = "10.10.12.0/24"
+#        az   = "eu-north-1a"
+#        #       nat_gateway                         = "AZ"  # it is default . you can remove it
+#      }
+#      "private3" = {
+#        name = "private-subnet-3"
+#        cidr = "10.10.13.0/24"
+#        az   = "eu-north-1b"
+#        #       nat_gateway                         = "AZ"  # it is default . you can remove it
+#      }
+#      "private4" = {
+#        name = "private-subnet-4"
+#        cidr = "10.10.14.0/24"
+#        az   = "eu-north-1b"
+#        #       nat_gateway                         = "AZ"  # it is default . you can remove it
+#      }
+#
 
     }
   }
--- examples/nat_gateway_az/output.tf
+++ examples/nat_gateway_az/output.tf
@@ -1,3 +1,7 @@
 output "nat_gateway_az_raw" {
   value = module.vpc.nat_gateway_az_raw
 }
+
+module "public_subnets_by_az" {
+  source = module.vpc.public_subnets_by_az
+}
