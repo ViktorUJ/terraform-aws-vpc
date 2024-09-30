1a9413eb07557295994709a5a998bf911c1da653
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-30 21:14:03 +0400

    ~

--- examples/nat_gateway_az/output.tf
+++ examples/nat_gateway_az/output.tf
@@ -2,6 +2,6 @@ output "nat_gateway_az_raw" {
   value = module.vpc.nat_gateway_az_raw
 }
 
-module "public_subnets_by_az" {
-  source = module.vpc.public_subnets_by_az
+output "public_subnets_by_az" {
+  value = module.vpc.public_subnets_by_az
 }
